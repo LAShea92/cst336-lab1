127780eba1cdadac055c6e08a550ca254dad9ac8
lshea <lshea@csumb.edu>
2019-07-02 23:06:56 -0400

    fixed some stuff

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ in html
         </header>
         <hr />
         <nav>
-          <a href="index.php">Home</a>
+          <a id="active" href="index.php">Home</a>
           <a href="about.html">About</a>
           <a href="contact.html">Contact</a>
         </nav>
